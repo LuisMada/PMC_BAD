4d7c5d3f7d36e4f789dfc98a481234f8c2f34dfe
LuisMada <luis.c.madrid@student.ateneo.edu>
2025-04-01 18:04:32 +0800

    more debugging media

--- VehicleManagementSystem/views.py
+++ VehicleManagementSystem/views.py
@@ -1,5 +1,6 @@
 import datetime
 import os
+from django.conf import settings
 from django.urls import reverse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
